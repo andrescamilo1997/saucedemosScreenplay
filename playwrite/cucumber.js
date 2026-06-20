12898fc3437b4dbcb49fa110efcfc759b20d2e00
camilo.diaz <camilo.diaz@sofka.com.co>
2026-06-20 17:31:56 -0500

    Ajuste YML

--- playwrite/cucumber.js
+++ playwrite/cucumber.js
@@ -1,12 +1,12 @@
-// cucumber.js
 export default {
-  paths: ['tests/features/**/*.feature'],
-  import: [
-    'tests/step-definitions/**/*.js',
-    'tests/hooks/**/*.js'
-  ],
-  format: [
-    'summary',
-    'html:reports/cucumber-report.html'
-  ]
+  default: {
+    parallel: 1,
+    format: [
+      'summary',
+      'json:reports/cucumber_report.json',
+      'html:reports/cucumber-report.html' // 🚀 ¡ESTA LÍNEA HACE TODO EL TRABAJO!
+    ],
+    paths: ['tests/features/**/*.feature'],
+    import: ['tests/step-definitions/**/*.js', 'tests/support/**/*.js'] // Ajusta según tus rutas de hooks
+  }
 };
